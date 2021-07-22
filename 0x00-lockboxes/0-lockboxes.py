0a5d7bdd220236aa369b38582eb8c68106bc5cf6
Hadir Jenni <2063@holbertonschool.com>
2021-07-22 18:50:11 +0100

    Update

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ canUnlockAll function"""
+
+
 def canUnlockAll(boxes):
     """ determines if all the boxes can be opened."""
     KeysLs = [0]
@@ -7,7 +9,6 @@ def canUnlockAll(boxes):
         for value in boxes[key]:
             if value not in KeysLs and value < len(boxes):
                 KeysLs.append(value)
-    for i in range(len(boxes)):
-        if i not in KeysLs:
-            return False
+    if len(boxes) != len(KeysLs):
+        return False
     return True
