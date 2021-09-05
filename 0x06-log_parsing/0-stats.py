e3b4bb13713f1cc6f627107acb8d567f6dbfe8ef
Hadir Jenni <2063@holbertonschool.com>
2021-09-05 15:02:20 +0100

    [Update]

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,10 +4,10 @@ Script that reads stdin line by line and computes metrics
 """
 import sys
 
-size = 0
+sizes = 0
 counter = 0
 
-dict_codes_counter = {'200': 0, '301': 0, '400': 0, '401': 0,
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
                       '403': 0, '404': 0, '405': 0, '500': 0}
 
 try:
@@ -18,12 +18,12 @@ try:
             size = int(line_list[-1])
             if code in dict_codes_counter.keys():
                 dict_codes_counter[code] += 1
-            size += size
+            sizes += size
             counter += 1
 
         if counter == 10:
-            print("File size: {:d}".format(total_size))
-            for k, v in sorted(dict_codes_counter.items()):
+            print("File size: {:d}".format(sizes))
+            for k, v in sorted(status_codes.items()):
                 if v != 0:
                     print("{}: {:d}".format(k, v))
             counter = 0
@@ -32,7 +32,7 @@ except Exception:
     pass
 
 finally:
-    print("File size: {}".format(total_size))
-    for k, v in sorted(dict_codes_counter.items()):
+    print("File size: {}".format(sizes))
+    for k, v in sorted(status_codes.items()):
         if v != 0:
             print("{}: {}".format(k, v))
