b12cc88fdbd608719779cc90bf525435d9982de7
Hadir Jenni <hadir.jenni@gmail.com>
2021-10-10 19:28:31 +0100

    [ADD]

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,36 @@
+#include "menger.h"
+/**
+* menger - draws a 2D Menger Sponge
+* @level: level of the Menger Sponge to draw
+* Rturn: void
+**/
+void menger(int level)
+{
+	int i, j, size, row, col;
+	char c;
+
+	if (level >= 0)
+    {
+        size = pow(3, level);
+	    for (i = 0; i < level_size; i++)
+	    {
+		    for (j = 0; j < level_size; j++)
+		    {
+			    c = '#';
+			    row = i;
+			    col = j;
+			    while (row != 0 || col != 0)
+			    {
+				    if ((row % 3 == 1) && (col % 3 == 1))
+				    {
+					    c = ' ';
+				    }
+				    row /= 3;
+				    col /= 3;
+			    }
+			    printf("%c", c);
+		    }
+		    printf("\n");
+	    }
+    }
+}
---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,10 @@
+#ifndef MENGER_SPONGE
+#define MENGER_SPONGE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void menger(int level);
+
+#endif
