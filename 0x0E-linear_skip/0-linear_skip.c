9a6fee72f7c372e8ef524cde882d6effd33541f4
Hadir Jenni <hadir.jenni@gmail.com>
2021-11-05 12:54:53 +0100

    Update 0-linear_skip.c

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -8,39 +8,38 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    if (list != NULL)
-    {
-        
-        skiplist_t *tmp, *nextTmp;
-        tmp = list;
-        nextTmp = tmp->express;
-        while (nextTmp != NULL)
-        {
-            printf("Value checked at index [%ld] = [%d]\n", nextTmp->index, nextTmp->n);
-            if (nextTmp->n < value)
-            {
-                tmp = tmp->express;
-                nextTmp = nextTmp->express;
-            }
-            else if(value <= nextTmp->n)
-            {
-                printf("Value found between indexes [%ld] and [%ld]\n", tmp->index, nextTmp->index);
-                while (tmp->index <= nextTmp->index)
-                {
-                    printf("Value checked at index [%ld] = [%d]\n", tmp->index, tmp->n);
-                    if (tmp->n == value)
-                    {
-                        return(tmp);
+	if (list != NULL)
+	{
+		skiplist_t *tmp, *nextTmp;
+		tmp = list;
+		nextTmp = tmp->express;
+		while (nextTmp != NULL)
+		{
+			printf("Value checked at index [%ld] = [%d]\n", nextTmp->index, nextTmp->n);
+			if (nextTmp->n < value)
+			{
+				tmp = tmp->express;
+				nextTmp = nextTmp->express;
+			}
+			else if(value <= nextTmp->n)
+			{
+				printf("Value found between indexes [%ld] and [%ld]\n", tmp->index, nextTmp->index);
+				while (tmp->index <= nextTmp->index)
+				{
+					printf("Value checked at index [%ld] = [%d]\n", tmp->index, tmp->n);
+					if (tmp->n == value)
+					{
+						return(tmp);
 
-                    }
-                    tmp = tmp->next;
-                } 
-            }
-            else
-            {
-                return(tmp);
-            }           
-        }
-    }
-    return NULL;
+					}
+					tmp = tmp->next;
+				}
+			}
+			else
+			{
+				return(tmp);
+			}
+		}
+	}
+	return NULL;
 }
