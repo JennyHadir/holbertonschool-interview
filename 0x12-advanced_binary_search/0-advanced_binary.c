8384fbc34a33590ec948c0421f6b25edab69c511
Hadir Jenni <hadir.jenni@gmail.com>
2021-12-02 12:50:47 +0100

    Fix Betty

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -7,47 +7,48 @@
  * @array: is a pointer to the first element of the array to search in
  * @size: is the number of elements in array
  * @value: is the value to search for
+ *
  * Return: return the index where value is located, -1 otherwise
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	if (array && size > 1)
-		return (recursion_advanced_binary(array, 0, size - 1, value));
+	if (array == NULL || size < 1)
+		return (-1);
 
-	return (-1);
+	return (recur_search(array, 0, size - 1, value));
 }
 
 /**
- * recursion_advanced_binary - recursive search for a value in a sorted array of integers
+ * recur_search - recursive search for a value in a sorted array of integers
  * @array: is a pointer to the first element of the array to search in
  * @first: is the first element of the array
  * @last: is the last element of the array
- * @value: is the value to search for
+ * @value: value to search for
+ *
  * Return: return the index where value is located, -1 otherwise
  */
-int recursion_advanced_binary(int *array, int first, int last, int value)
+int recur_search(int *array, int first, int last, int value)
 {
-	int middle = (first + last) / 2;
+	int mid = (first + last) / 2;
 	int i;
 
 	if (first > last)
 		return (-1);
-
 	printf("Searching in array: ");
+
 	for (i = first; i <= last; i++)
 	{
-		printf("%d", array[i]);
 		if (i < last)
-			printf(", ");
+			printf("%d, ", array[i]);
 		else
-			printf("\n");
+			printf("%d\n", array[i]);
 	}
 
 	if (array[first] == value)
 		return (first);
 
-	if (array[middle] < value)
-		return (recursion_advanced_binary(array, middle + 1, last, value));
+	if (array[mid] < value)
+		return (recur_search(array, mid + 1, last, value));
 
-	return (recursion_advanced_binary(array, first, middle, value));
+	return (recur_search(array, first, mid, value));
 }
--- 0x12-advanced_binary_search/search_algos.h
+++ 0x12-advanced_binary_search/search_algos.h
@@ -5,6 +5,6 @@
 #include <stdlib.h>
 
 int advanced_binary(int *array, size_t size, int value);
-int recursion_advanced_binary(int *array, int first, int last, int value);
+int recur_binary(int *array, int first, int last, int value);
 
 #endif /* SEARCH_ALGOS_H */
