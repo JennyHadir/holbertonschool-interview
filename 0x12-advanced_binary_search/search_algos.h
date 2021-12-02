e207bb16ccb27bb2e0510a64d4fc12b72f382466
Hadir Jenni <hadir.jenni@gmail.com>
2021-12-02 12:32:07 +0100

    Update

--- 0x12-advanced_binary_search/search_algos.h
+++ 0x12-advanced_binary_search/search_algos.h
@@ -1,6 +1,10 @@
-#ifndef SEARCH_Algos_H
-#define SEARCH_Algos_H
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int advanced_binary(int *array, size_t size, int value);
+int binary_search(int *array, int start, int end, int value);
 
 #endif /* SEARCH_ALGOS_H */
