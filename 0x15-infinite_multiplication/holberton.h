ce89cdb8cb994129237e7bf27e50c27ea5289f13
Hadir Jenni <hadir.jenni@gmail.com>
2022-01-11 20:17:48 +0100

    Update

--- 0x15-infinite_multiplication/holberton.h
+++ 0x15-infinite_multiplication/holberton.h
@@ -1,9 +1,9 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-#include <stdio.h>
-#include <unistd.h>
+#ifndef holberton_h
+#define holberton_h
 #include <stdlib.h>
-
 int _putchar(char c);
-
+int _strlen(char *str);
+void _print_error(void);
+int _isDigit(char c);
+void _mul(char *nb, char *nb2, int nblength, int nb2length);
 #endif
