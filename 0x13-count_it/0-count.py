45577abdc4ac147d50a6bf9bff924213a1082004
Hadir Jenni <hadir.jenni@gmail.com>
2022-01-12 12:23:39 +0100

    Remove duplicat

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,40 +1,53 @@
 #!/usr/bin/python3
-"""Get request to reddit API"""
+"""
+    Parses the title of all hot articles, and prints a sorted
+    count of given keywords (case-insensitive, delimited by spaces.
+"""
+import json
+import re
 import requests
+import time
 
 
-def count_words(subreddit, word_list, first_call=True, after="", dic={}):
-    """Cound # of keywords"""
-    if first_call:
-        word_list = list(set(word_list))
-        for i, e in enumerate(word_list):
-            word_list[i] = e.lower()
-        d = count_words(subreddit, word_list, False)
-        if d:
-            for key, value in sorted(d.items(), key=lambda x: (-x[1], x[0])):
-                print("{}: {}".format(key, value))
-        elif d is None:
-            return None
+def count_elements(request, word_list, results):
+    """ Counts number of elements """
+    for title in request['data']['children']:
+        datas = title['data']['title'].split(" ")
+        for i in range(len(datas)):
+            datas[i] = datas[i].lower()
+            if (datas[i] in word_list):
+                if (datas[i] in results.keys()):
+                    results[datas[i]] += word_list.count(datas[i])
+                else:
+                    results[datas[i]] = word_list.count(datas[i])
+    return results
+
+
+def count_words(subreddit, word_list, results={}, param={'limit': 100}):
+    """ Main function to count and print the words """
+    baseLink = 'https://api.reddit.com/r/%s/hot.json' % subreddit
+
+    if ('after' not in param):
+        word_list = [str.lower() for str in word_list]
+
+    link = baseLink
+    customHeaders = {'User-agent': 'HolbertonSchoolTask'}
+    r = requests.get(link, headers=customHeaders,
+                     params=param, allow_redirects=False)
+
+    if (r.status_code != 200):
+        return
+    data = r.content
+
+    data = json.loads(data.decode('utf-8'))
+    param = {'limit': 100, 'count': 100, 'after': data['data'].get('after')}
+    if (data['data'].get('after') is None):
+        results = count_elements(data, word_list, results)
+        results = sorted(
+            results.items(), key=lambda x: (-x[1], x[0]), reverse=False
+            )
+        for i in results:
+            print("{}: {}".format(i[0], i[1]))
     else:
-        base_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-        h = {'User-Agent': 'Reddit API test'}
-        params = {'limit': 200, 'after': after}
-        r = requests.get(base_url, headers=h,
-                         allow_redirects=False, params=params)
-        if r.status_code != 200:
-            return None
-        d = r.json()
-        if after is None:
-            return dic
-        l = d.get('data', {}).get('children')
-        for i in l:
-            title = i.get('data', {}).get('title').lower().split()
-            for j in word_list:
-                for t in title:
-                    if j == t:
-                        if j not in dic:
-                            dic[j] = 1
-                        else:
-                            dic[j] += 1
-        p = d.get('data', {}).get('after')
-        return count_words(subreddit, word_list, False, p, dic)
+        results = count_elements(data, word_list, results)
+        count_words(subreddit, word_list, results, param)
