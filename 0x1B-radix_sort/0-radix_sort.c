f8da66859c550ad4c454ed465ee6839414967656
Hadir Jenni <hadir.jenni@gmail.com>
2022-02-16 16:14:40 +0100

    Update: fix errors

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -1,73 +1,74 @@
-#include "sort.h"
-
 /**
- * getMax - get max value
- * @array: array contains values
- * @n: size of array
- * Return: max number of array
- */
-int getMax(int array[], int n)
+* getMax - get max value
+* @array:array to be sorted
+* @n: size of array
+* Return: integer, max number of array
+*/
+int getMax(int *array, int n)
 {
-  	int i, max;
+	int i, max;
 
 	for (max = array[0], i = 1; i < n; i++)
 		if (array[i] > max)
 			max = array[i];
 	return (max);
 }
-
 /**
- * countingSort - counting sort according to the digit
- * @array: array to be sorted
- * @size: size of array
- * @place: placeof digits
- * Return: None
- */
-void countingSort(int array[], int size, int place)
+* countSort - counting sort of arr[] according to the digit represented by exp
+* @array:array to be sorted
+* @n:size of array
+* @exp: exponential
+* Return - None
+*/
+void countSort(int *array, int n, int exp)
 {
 	int *output, i, count[10] = { 0 };
 
-	output = malloc(sizeof(int) * size);
+	output = malloc(sizeof(int) * n);
 	if (output == NULL)
 		return;
-	// Calculate count of elements
-	for (int i = 0; i < size; i++)
-	{
-		count[(array[i] / place) % 10]++;
-	}
-	// Calculate cumulative count
-	for (int i = 1; i < 10; i++)
+	/*Store count of occurrences in count[]*/
+	for (i = 0; i < n; i++)
+		count[(array[i] / exp) % 10]++;
+
+	/*Change count[i] so that count[i] now contains actual*/
+	/*position of this digit in output[]*/
+	for (i = 1; i < 10; i++)
 		count[i] += count[i - 1];
 
-	// Place the elements in sorted order
-	for (int i = size - 1; i >= 0; i--)
+	/*Build the output array*/
+	for (i = n - 1; i >= 0; i--)
 	{
-		output[count[(array[i] / place) % 10] - 1] = array[i];
-		count[(array[i] / place) % 10]--;
+		output[count[(array[i] / exp) % 10] - 1] = array[i];
+		count[(array[i] / exp) % 10]--;
 	}
-	for (int i = 0; i < size; i++)
+
+	/*Copy the output array to arr[], so that arr[] now*/
+	/*contains sorted numbers according to current digit*/
+	for (i = 0; i < n; i++)
 		array[i] = output[i];
 	free(output);
 }
-
 /**
- * radix_sort - Sort an array with radix sort
- * @array: array to be sorted
- * @size: size of array
- * Return: void
- */
+* radix_sort - sort an array
+* @array:array to be sorted
+* @size:size of array
+* Return - None
+*/
 void radix_sort(int *array, size_t size)
 {
-	int place, max;
+	/*Find the maximum number to know number of digits*/
+	int exp, m;
 
 	if (size < 2)
 		return;
-	// Get maximum element
-	max = getMax(array, size);
-	// Apply counting sort to sort elements based on place value
-	for (place = 1; max / place > 0; place *= 10)
+	m = getMax(array, size);
+	/* Do counting sort for every digit. Note that instead */
+	/* of passing digit number, exp is passed. exp is 10^i */
+	/* where i is current digit number */
+	for (exp = 1; m / exp > 0; exp *= 10)
 	{
-		countingSort(array, size, place);
+		countSort(array, size, exp);
 		print_array(array, size);
 	}
 }
